73c5fabf0a546acb325dbe23a6b41274424a8702
Daniil Dulin <daniildulin@gmail.com>
2018-10-22 18:04:30 +0300

    init commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/daniildulin/explorer-extender/env"
+	"os"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var Version string   // Version
+var GitCommit string // Git commit
+var BuildDate string // Build date
+var AppName string   // Application name
+var config env.Config
+
+var version = flag.Bool(`v`, false, `Prints current version`)
+
+// Initialize app.
+func init() {
+	config = env.NewViperConfig()
+	AppName = config.GetString(`name`)
+	Version = `0.1`
+
+	if config.GetBool(`debug`) {
+		fmt.Println(`Service RUN on DEBUG mode`)
+	}
+}
+
+func main() {
+
+	flag.Parse()
+
+	if *version {
+		fmt.Printf(`%s v%s Commit %s builded %s\n`, AppName, Version, GitCommit, BuildDate)
+		os.Exit(0)
+	}
+}
